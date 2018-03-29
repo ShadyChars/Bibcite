ebc808ae5dc7866a092e2d7b78005553bb9f9ca3
Keith Houston <keith.houston@gmail.com>
2018-03-29 21:48:35 +0100

    * Finally got ETag-based conditional fetching to work * Tightened up handling of date/times when downloading the library

--- includes/class-bibcite-library.php
+++ includes/class-bibcite-library.php
@@ -7,29 +7,28 @@ class Bibcite_Library
 	// TODO: make this a setting.
 	const LIBRARY_URL = 'https://www.dropbox.com/s/m1lgya889qnz081/library.bib?dl=1';
 
-	// Prefix used for all our transient values.
+	// Prefix used for all our transient and option values.
 	const BIBCITE_PREFIX = 'BIBCITE_SC';
 
 	// Option names.
-	const OPTION_LAST_UPDATED = Bibcite_Library::BIBCITE_PREFIX . "_LAST_UPDATED";
+	const OPTION_LAST_ETAG = Bibcite_Library::BIBCITE_PREFIX . "_LAST_ETAG";
+	const OPTION_LAST_DOWNLOADED_TIME = Bibcite_Library::BIBCITE_PREFIX . "_LAST_DOWNLOADED_TIME";
 
 	// How long should we wait between HTTP requests?
-	private static $HTTP_DORMANCY;
+	const HTTP_DORMANCY_SECONDS = 300;
 
-	// Hold an instance of the class
+	// Hold an instance of the class.
 	private static $instance;
 
 	// Option values, if known.
-	private $last_updated;
+	private $last_etag;
+	private $last_downloaded_time;
 
 	// The singleton method
 	public static function instance()
 	{
 		if (!isset(self::$instance)) {
 			self::$instance = new Bibcite_Library();
-
-			// Static initialisation.
-			self::$HTTP_DORMANCY = new DateInterval("PT5S");
 		}
 		return self::$instance;
 	}
@@ -40,14 +39,18 @@ class Bibcite_Library
 	private function __construct() {
 
 		// Read options, if available.
-		$last_updated = get_option( Bibcite_Library::OPTION_LAST_UPDATED );
-		if ( $last_updated ) {
-			$this->last_updated = new DateTime($last_updated);
+		$this->last_etag = get_option( Bibcite_Library::OPTION_LAST_ETAG );
+		if ( $this->last_etag != false ) {
+			Bibcite_Logger::instance()->info( "Last library ETag: " . $this->last_etag );
 		}
 
-		Bibcite_Logger::instance()->info(
-			"Library last updated: " . var_export($this->last_updated, true)
-		);
+		$last_downloaded_string = get_option( Bibcite_Library::OPTION_LAST_DOWNLOADED_TIME );
+		if ( $last_downloaded_string != false) {
+			$this->last_downloaded_time = intval( $last_downloaded_string );
+			Bibcite_Logger::instance()->info(
+				"Library last downloaded: " . date ( DATE_RFC850, $this->last_downloaded_time )
+			);
+		}		
 	}
 
 	/**
@@ -58,10 +61,11 @@ class Bibcite_Library
 		// Make sure our library is up to date.
 		$this->update_library();
 
-		// Do we have the requested item as a transient?
+		// Do we have the requested item as a transient? 
+		// KHFIXME: should we use a proper DB instead? We don't necessarily want these to expire.
 		$transient_key = Bibcite_Library::BIBCITE_PREFIX . "_" . $bibtex_key;
 		$bibtex_value = get_transient ( $transient_key );
-		if ( $bibtex_value !== false )		
+		if ( $bibtex_value !== false )
 			return $bibtex_value;
 		else
 			return null;
@@ -72,66 +76,103 @@ class Bibcite_Library
 	 */
 	private function update_library() {
 
-		// If we last updated our library within the specifed $HTTP_DORMANCY period, do nothing.
-		if ( $this->last_updated && 
-			($this->last_updated < (new DateTime() - self::$HTTP_DORMANCY))
+		// If we last updated our library within the specifed HTTP_DORMANCY period, do nothing.
+		if ( isset($this->last_downloaded_time) && 
+			( $this->last_downloaded_time >= (time() - Bibcite_Library::HTTP_DORMANCY_SECONDS ) )
 		) {
 			Bibcite_Logger::instance()->debug(
-				"Library last updated within " . self::$HTTP_DORMANCY . ". Skipping update."
+				"Library last downloaded within " . 
+				Bibcite_Library::HTTP_DORMANCY_SECONDS .
+				" seconds. Skipping update."
 			);
 			return;
 		}
 
-		// We need to update the library. Get the headers for the library and work out if it's 
-		// changed since we last updated it. 
-		// create curl resource 
-		$library_headers = null;
-		try {
-			Bibcite_Logger::instance()->info(
-				"Library has not been updated in the past " . 
-				self::$HTTP_DORMANCY->format( '%H:%I:%S' ) . 
-				". Getting headers from " .
-				Bibcite_Library::LIBRARY_URL . 
-				"..."
-			);
+		Bibcite_Logger::instance()->info( 
+			"Updating library from URL (" . Bibcite_Library::LIBRARY_URL . ")..." 
+		);
 
+		// Get the library, but only if it has an ETag different from the last one we saw.
+		$library_body = null;
+		$library_headers = [];
+		$http_code = null;
+		$curl_error = null;
+		try {
 			$ch = curl_init(); 
 			curl_setopt($ch, CURLOPT_URL, Bibcite_Library::LIBRARY_URL); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);		// Return rather than log the result
-			curl_setopt($ch, CURLOPT_FILETIME, true);			// Get the time the resource changed
-			curl_setopt($ch, CURLOPT_NOBODY, true);				// Exclude the body
-			curl_setopt($ch, CURLOPT_HEADER, true);				// Include the headers
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);		// Follow redirection
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);	// Ignore SSL certs
-			$library_headers = curl_exec($ch); 			
-			$curl_error = curl_error($ch);
-			$curl_info = curl_getinfo($ch);
-			curl_close($ch);
 
-			// If there's an error, report as much info as possible.
-			if ($curl_error) {
-				Bibcite_Logger::instance()->error("Curl error: ${curl_error}");	
-				if ($curl_info)
-					Bibcite_Logger::instance()->error("Curl info: ". var_export($curl_info, true));	
-			}
+			// Capture headers.
+			curl_setopt(
+				$ch, 
+				CURLOPT_HEADERFUNCTION,
+				function($curl, $header) use (&$library_headers)
+				{
+					$len = strlen($header);
+					$header = explode(':', $header, 2);
+					if (count($header) < 2) // ignore invalid headers
+						return $len;
+
+					$name = strtolower(trim($header[0]));
+					$value = trim($header[1]);
+					if (!array_key_exists($name, $library_headers))
+						$library_headers[$name] = [$value];
+					else
+						$library_headers[$name][] = $value;
+
+					return $len;
+				}
+			);
+
+			// If we don't have an ETag, we'll get the complete library.
+			if ( $this->last_etag )
+				curl_setopt( $ch, CURLOPT_HTTPHEADER, array ( "If-None-Match: $this->last_etag" ) );
+			
+			$library_body = curl_exec($ch);		
+			$curl_error = curl_error($ch);
+			$http_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+			curl_close($ch);
 
 		} catch (Exception $e) {
 			Bibcite_Logger::instance()->error(
-				"Failed to retrieve library from URL (" . Bibcite_Library::LIBRARY_URL . "): " . 
+				"Failed to get library from URL (" . Bibcite_Library::LIBRARY_URL . "): " . 
 				$e->getMessage() . 
 				" Using cached library entries."
 			);
 			return;
 		}
 
-		// Has the library changed since we last downloaded it? If not, just use it as is.
-		Bibcite_Logger::instance()->debug(
-			"Library headers: \n" . var_export($library_headers, true)
-		);
-		
-		// Download the full library and parse it.
-		
-		// Update our last-updated date/time.
+		// If there's an error, report as much info as possible and quit now.
+		if ($curl_error) {
+			Bibcite_Logger::instance()->error(
+				"Error getting library: ${curl_error}. Using cached library entries."
+			);					
+			return;
+		}
 
+		// The request succeeded. Update our ETag and our last GET datetime.
+		$this->last_etag = 
+			array_key_exists( "etag", $library_headers ) ? $library_headers["etag"][0] : null;
+		$this->last_updated_datetime = time();
+		update_option( Bibcite_Library::OPTION_LAST_ETAG, $this->last_etag );
+		update_option( Bibcite_Library::OPTION_LAST_DOWNLOADED_TIME, $this->last_updated_datetime );
+
+		$last_updated_string = date ( DATE_RFC850, $this->last_updated_datetime	);
+		Bibcite_Logger::instance()->debug( 
+			"Writing Etag ($this->last_etag) and library update time ($last_updated_string)."
+		);
+
+		// Did we get a new copy of the library? If not, nothing more to do.
+		if ( strlen( $library_body ) <= 0 ) {
+			Bibcite_Logger::instance()->debug( "No new library received. Using existing library." );
+			return;
+		}
+
+		// Save the new library and parse it into the DB.
+		Bibcite_Logger::instance()->debug( "Received new library. Parsing..." );
+
+		// KHFIXME: TK
 	}
 }
